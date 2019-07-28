da72a49d22f212be8c13916bd0215f4dc4e8679e
Axel Pauwels <axel.pauwels@phpro.be>
2019-07-28 15:13:47 +0200

    JSexample-2 LOADING JAVASCRIPT IN A PHTML TEMPLATE WITH REQUIREJS VIA X-MAGENTO-INIT

--- src/app/code/VendorAxel/ModuleJs/view/frontend/web/js/script-example1.js
+++ src/app/code/VendorAxel/ModuleJs/view/frontend/web/js/script-example1.js
@@ -1,2 +1,2 @@
 //https://www.mage2.tv/content/javascript/requirejs-fundamentals/loading-javascript-in-a-page-with-layout-xml/
-console.log('script-example');
+console.log('script-example1');
---
+++ src/app/code/VendorAxel/ModuleJs/view/frontend/web/js/script-example2-requirejs.js
@@ -0,0 +1,4 @@
+// https://www.mage2.tv/content/javascript/requirejs-fundamentals/loading-javascript-in-a-phtml-template-with-requirejs-via-x-magento-init
+
+// magento uses require js to load this file
+console.log('script-example2, I am loaded through magento by require-js');
