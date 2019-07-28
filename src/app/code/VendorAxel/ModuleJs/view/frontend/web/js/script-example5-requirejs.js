19ec84368d179971a37e4a7b36d5a0ea5dbe7520
Axel Pauwels <axel.pauwels@phpro.be>
2019-07-28 15:55:21 +0200

    JSexample-5 TARGETING DOM NODES IN SIMPLE REQUIREJS MODULES

---
+++ src/app/code/VendorAxel/ModuleJs/view/frontend/web/js/script-example5-requirejs.js
@@ -0,0 +1,14 @@
+// https://www.mage2.tv/content/javascript/requirejs-fundamentals/targeting-dom-nodes-in-simple-requirejs-modules/
+// ---
+// THIS FILE: "select a specific dom node"
+
+define(function () {
+
+    // receive the element (that is selected by the selector in phtml)
+    // if the selector is an asterisk, this function will be called once and element will be "false"
+    return function (config, element) {
+        console.log('script-example5, I am targetting a specific dom node');
+        console.log(config);
+        console.log(element);
+    }
+});
