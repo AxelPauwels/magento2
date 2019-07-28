df7490cf04a56febf00ec2dbc2f0fe43dcf7f7fd
Axel Pauwels <axel.pauwels@phpro.be>
2019-07-28 14:43:37 +0200

    JSexample-1 LOADING JAVASCRIPT IN A PAGE WITH LAYOUT XML

---
+++ src/app/code/VendorAxel/ModuleJs/view/frontend/web/js/script-example1.js
@@ -0,0 +1,2 @@
+//https://www.mage2.tv/content/javascript/requirejs-fundamentals/loading-javascript-in-a-page-with-layout-xml/
+console.log('script-example');
---
+++ src/dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+    axel_us: {
+        area: 'frontend',
+        name: 'VendorAxel/ThemeAxel',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    blank: {
+        area: 'frontend',
+        name: 'Magento/blank',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+    luma: {
+        area: 'frontend',
+        name: 'Magento/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    backend: {
+        area: 'adminhtml',
+        name: 'Magento/backend',
+        locale: 'en_US',
+        files: [
+            'css/styles-old',
+            'css/styles'
+        ],
+        dsl: 'less'
+    }
+};
