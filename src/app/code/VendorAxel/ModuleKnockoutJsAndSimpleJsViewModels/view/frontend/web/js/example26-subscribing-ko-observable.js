da472af21c02d5eb77bfe1d592365fbfed589cd6
Axel Pauwels <axel.pauwels@phpro.be>
2019-08-04 19:25:44 +0200

    JSexample-26 SUBSCRIBING TO CHANGES OF KNOCKOUTJS OBSERVABLES

---
+++ src/app/code/VendorAxel/ModuleKnockoutJsAndSimpleJsViewModels/view/frontend/web/js/example26-subscribing-ko-observable.js
@@ -0,0 +1,44 @@
+// Video: https://www.mage2.tv/content/javascript/knockoutjs-and-simple-javascript-view-models/subscribing-to-changes-of-knockoutjs-observables/
+// Video description:
+// Besides triggering the UI to automatically update when a value changes,
+// observables can also be used to notify subscribers when updates occur.
+
+// EXAMPLE 1 (subscribe with callback to receive the newValue)
+
+// define(['ko'], function (ko) {
+//     'use strict';
+//     console.log('* example 26 *');
+//
+//     return function (config) {
+//         const title = ko.observable('This is a title for a view model');
+//         title.subscribe(function (newValue) {
+//             console.log('-> The new value = ', newValue);
+//         });
+//         return {
+//             title: title
+//         }
+//     }
+// });
+
+// EXAMPLE 2 (subscribe with callback to receive the newValue AND oldValue)
+
+define(['ko'], function (ko) {
+    'use strict';
+    console.log('* example 26 *');
+
+    return function (config) {
+        const title = ko.observable('This is a title for a view model');
+
+        title.subscribe(function (oldValue) {
+            console.log('-> The old value = ', oldValue);
+        }, this, 'beforeChange'); // note this is bind to "this"
+
+        title.subscribe(function (newValue) {
+            console.log('-> The new value = ', newValue);
+        });
+
+        return {
+            title: title
+        }
+    }
+});
