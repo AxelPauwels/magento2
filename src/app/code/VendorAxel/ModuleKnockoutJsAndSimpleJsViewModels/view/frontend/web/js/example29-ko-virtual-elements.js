86051a3544431f62437fbb3c5d88968d6d762f52
Axel Pauwels <axel.pauwels@phpro.be>
2019-08-05 08:31:51 +0200

    JSexample-29 KNOCKOUTJS VIRTUAL ELEMENTS

---
+++ src/app/code/VendorAxel/ModuleKnockoutJsAndSimpleJsViewModels/view/frontend/web/js/example29-ko-virtual-elements.js
@@ -0,0 +1,21 @@
+// Video: https://www.mage2.tv/content/javascript/knockoutjs-and-simple-javascript-view-models/knockoutjs-virtual-elements/// Video description:
+// Having to insert DOM nodes for control flow bindings can be a nuisance.
+// Virtual elements allow hiding them from the rendered DOM as HTML comments.
+
+define(['ko'], function (ko) {
+    'use strict';
+    console.log('* example 29 *');
+
+    return function () {
+        const viewModel = {
+            exchange_rates: ko.observableArray([
+                {
+                    currency_to: 'EUR',
+                    rate: 1.0
+                }
+            ])
+        };
+
+        return viewModel;
+    }
+});
