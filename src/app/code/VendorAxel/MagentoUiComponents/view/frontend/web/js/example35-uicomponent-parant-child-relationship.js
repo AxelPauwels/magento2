173f789eca26ecd96d306b19ebbb73abae91658b
Axel Pauwels <axel.pauwels@phpro.be>
2019-08-05 11:24:50 +0200

    JSexample-36 SELECTING UICOMPONENTS WITH THE UIREGISTRY

--- src/app/code/VendorAxel/MagentoUiComponents/view/frontend/web/js/example35-uicomponent-parant-child-relationship.js
+++ src/app/code/VendorAxel/MagentoUiComponents/view/frontend/web/js/example35-uicomponent-parant-child-relationship.js
@@ -2,16 +2,3 @@
 // Video description:
 // Most of the time UiComponents are built as a hierarchy of related elements.
 // This video discusses how to define the hierarchy and how the UiComponents can relate to each other.
-
-define(['uiElement'], function (UiElement) {
-    'use strict';
-    console.log('* example 35 *');
-
-    return UiElement.extend({
-        defaults: {
-            template: "VendorAxel_MagentoUiComponents/example34-ui-component-template",
-            label: "Random numbers",
-            values: [22, 33, 45, 2, 80]
-        },
-    });
-});
---
+++ src/app/code/VendorAxel/MagentoUiComponents/view/frontend/web/js/example36-selecting-uicomponents-with-uiregistry.js
@@ -0,0 +1,19 @@
+// Video: https://www.mage2.tv/content/javascript/knockoutjs-and-simple-javascript-view-models/selecting-uicomponents-with-the-uiregistry/
+// Video description:
+// When working with the Magento frontend architecture, it is very helpful to be able to query specific instances of
+// UiComponents on a page via the UiRegistry.
+
+// require('uiRegistry').get('myUiComponent')
+// require('uiRegistry').get('myUiComponent').name
+// require('uiRegistry').get('myUiComponent.child1').name
+// require('uiRegistry').get('myUiComponent.child2').name
+// require('uiRegistry').get('index = child2').name
+// require('uiRegistry').get('ns = myUiComponent').name   (returns only the first one (not all 3))
+// require('uiRegistry').get('ns = myUiComponent, index = child1').name   (combining)
+//
+// with callback function
+// require('uiRegistry').get('ns = myUiComponent, index = child2', function (c) {console.log(c.name)})
+// require('uiRegistry').get('function (c) {console.log(c.name)})
+//
+// get all
+// require('uiRegistry').get(function (c) {console.log(c.name)})
